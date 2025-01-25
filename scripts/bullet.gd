667ea08034b0de6e531d42db83db8a0b43fb2f21
Nadman Faisal <nadman@chalmers.se>
2025-01-25 16:13:56 +0100

    Bullets keep moving regardless of existence of mob

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,13 +4,14 @@ var constantMovementVelocity = Vector2(1, 0)
 @export var speed = 300
 @export var damage = 25
 var target_position = Vector2.ZERO
+var direction = Vector2.ZERO
 
 func fire(target: Vector2):
 	target_position = target
+	direction = (target_position - position).normalized()
 
 func _physics_process(delta):
 	if target_position != Vector2.ZERO:
-		var direction = (target_position - position).normalized()
 		var collision_info = move_and_collide(direction * speed * delta)
 		if collision_info:
 			var collided_object = collision_info.get_collider()

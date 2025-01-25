abc5cf3a958122273510f260cdcdfac12f3160ed
Nadman Faisal <nadman@chalmers.se>
2025-01-25 16:47:49 +0100

    Build tower wherever the player wants

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+@export var tower_scene: PackedScene
+var is_build_mode = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,4 +22,24 @@ func _on_the_bubble_killed() -> void:
 	
 func update_money(money) -> void:
 	$MoneyLabel.text = "Money: " + str(money)+"$"
+	
+
+
+func _on_build_tower_button_pressed() -> void:
+	is_build_mode = true
+	print("Build mode entered.")
+	
+func _input(event):
+	if is_build_mode and event is InputEventMouseButton and event.pressed:
+		place_tower(event.position)
+
+func place_tower(position: Vector2):
+	if not tower_scene:
+		print("No tower scene assigned.")
+		return
+	var tower = tower_scene.instantiate()
+	get_tree().current_scene.add_child(tower)
+	tower.global_position = position
+	print("Tower placed at: ", position)
+	is_build_mode = false
 	
